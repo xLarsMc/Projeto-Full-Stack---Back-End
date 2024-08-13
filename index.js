f619ae91a196b8beaf567b94fc842613e47357f2
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-08-13 07:25:50 -0300

    Adição do body-parser para configuração JSON

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+//Importações e configurações
 require("dotenv").config()
-
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Teste.");
-})
-
+//Leitura JSON/Configuração cors
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(cors());
 
+//Ligando o servidor
 app.listen(port, () => {
-    console.log("Working!")
+    console.log("Working! Port: " + port);
+})
+
+//Configurando rotas
+app.get("/teste", (req, res) => {
+    res.send("Teste.");
 })
 
 require("./Database/mongoConnection");
