6f689c6f6688b77be1fd19b7ed585ab430f2de8e
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-08-13 07:34:39 -0300

    Criação dos modelos user e post no banco de dados.

---
+++ Models/model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    login: String,
+    senha: String
+});
+
+const post = new mongoose.Schema({
+    name: String,
+    commonPlaces: String,
+    description: String,
+    drops: String
+})
+
+const userModel = mongoose.model("user", user);
+const postModel = mongoose.model("post", post);
+
+module.exports = {
+    userModel,
+    postModel
+}
